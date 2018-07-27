ea528a582ab04e5da1ed17cfaa375b277328f46b
Lucas Miqueias <Lucas@Lucass-MacBook-Pro.local>
2018-07-26 21:31:20 -0300

    updated deploy

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/list-filme'));
+app.use(express.static(__dirname + '/dist/filme-list'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/list-filme/index.html'));
+res.sendFile(path.join(__dirname+'/dist/filme-list/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
